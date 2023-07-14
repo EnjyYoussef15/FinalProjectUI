4f4e17d2a0d8816255e365e942ea903b1c0187e3
Osama Ibrahim <osama.ibrahim.sadek@gmail.com>
2023-07-14 22:51:19 +0300

    Favorites

--- src/app/unit/Components/favorites/favorites.component.ts
+++ src/app/unit/Components/favorites/favorites.component.ts
@@ -24,34 +24,18 @@ export class FavoritesComponent {
 
   constructor(private services:UnitService,private router:Router){}
   ngOnInit(): void {
-  this.getunitsComponent();
   this.getFavorites();
+  this.getAllFavorites();
 this.pageElement=this.totalCount/this.pageSize;
 this.buttonArray= Array(this.pageElement).fill(0).map((_, index) => index + 1);
-console.log("ButtonToArray",this.buttonArray);
 
-  }
-
-  getunitsComponent(){
-    this.services.getAllUnits(this.pageNumber,this.pageSize).subscribe(response => {
-      this.unitCard = response.data;
-      this.totalCount = response.totalCount;
-      console.log("UnitCard", this.unitCard);
-      console.log("TotalCount==>>",this.totalCount);
-      console.log("Count===>>>>",this.pageElement);
-
-this.pageElement=Math.floor(this.totalCount/this.pageSize)+(this.totalCount%this.pageSize>0?1:0);
-console.log("PageELement=====>>>" , this.pageElement);
-
-
-this.buttonArray= Array(this.pageElement).fill(0).map((_, index) => index + 1);
-console.log("ButtonToArray",this.buttonArray);
-    });
   }
 
   togleFavorites(id:number){
 if(this.checkFavorite(id)){
   this.favorites = this.favorites.filter(item => item.unitID !== id);
+
+  this.unitCard = this.unitCard.filter(item => item.id !== id);
   this.services.removeFavorites(id).subscribe();
 }
 else{
@@ -67,40 +51,65 @@ getFavorites(){
   this.services.getFavorites().subscribe({
     next:(value)=> {
       this.favorites=value;
-      console.log("Favorites==>> ",this.favorites);
 
     },
+  });
+}
+
+// getunitsComponent(){
+//   this.services.getAllUnits(this.pageNumber,this.pageSize).subscribe(response => {
+//     this.unitCard = response.data;
+//     this.totalCount = response.totalCount;
+//     console.log("UnitCard", this.unitCard);
+//     console.log("TotalCount",this.totalCount);
+//     console.log("Count",this.pageElement);
+
+//     this.pageElement=Math.floor(this.totalCount/this.pageSize)+(this.totalCount%this.pageSize>0?1:0);
+
+// this.buttonArray= Array(this.pageElement).fill(0).map((_, index) => index + 1);
+// console.log("ButtonToArray",this.buttonArray);
+//   });
+// }
+ getAllFavorites(){
+    this.services.getAllFavorites(this.pageNumber,this.pageSize).subscribe(response => {
+      this.unitCard = response.data;
+      this.totalCount = response.totalCount;
+      console.log("UnitCard", this.unitCard);
+      console.log("TotalCount",this.totalCount);
+      console.log("Count",this.pageElement);
+
+      this.pageElement=Math.floor(this.totalCount/this.pageSize)+(this.totalCount%this.pageSize>0?1:0);
+
+  this.buttonArray= Array(this.pageElement).fill(0).map((_, index) => index + 1);
   });
 }
 
 checkFavorite(id:any):boolean{
   for (let index = 0; index < this.favorites.length; index++) {
     if(this.favorites[index].unitID==id){
-          console.log("Favorites====> True ==");
           return true
     }
   }
-  console.log("Favorites====> False ==");
   return false;
 }
 
 previousPage() {
   if (this.pageNumber > 1) {
     this.pageNumber--;
-    this.getunitsComponent();
+    this.getAllFavorites();
   }
 }
 
 nextPage() {
   if (this.pageNumber * this.pageSize < this.totalCount) {
     this.pageNumber++;
-    this.getunitsComponent();
+    this.getAllFavorites();
   }
 }
 
 setPage(pnumber:number){
   this.pageNumber=pnumber;
-  this.getunitsComponent();
+  this.getAllFavorites();
 }
 
 }
